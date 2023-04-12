a57b4177307bad7e62960427b5ecc23c0e905e0e
Alina Zatzick <azatzick@middlebury.edu>
2023-04-12 11:18:29 -0400

    Homepage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,32 @@
+/* eslint-disable */
 import "../styles/globals.css";
+import { useState } from "react";
+import Head from "next/head";
+import { useEffect } from "react";
+import PropTypes from "prop-types";
+import styles from "../styles/MainApp.module.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { useRouter } from "next/router";
+
+function MainApp({ Component, pageProps }) {
+  const props = {
+    ...pageProps,
+  };
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Midd Markit</title>
+      </Head>
+      <main>
+        <Component {...props} />
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
+
+export default MainApp;
+MainApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.shape({}),
+};
