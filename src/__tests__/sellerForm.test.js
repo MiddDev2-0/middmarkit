4c7bda9be15e5c3f7641ee8a8b706e35f3a77e57
Alina Zatzick <azatzick@middlebury.edu>
2023-05-15 18:10:35 -0400

    seller page tests

--- src/__tests__/sellerForm.test.js
+++ src/__tests__/sellerForm.test.js
@@ -1,15 +1,56 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import SellerForm from "../components/SellerForm";
+import SellerPage from "@/pages/items/new";
+import fetchMock from "fetch-mock-jest";
+import { useSession, SessionProvider } from "next-auth/react";
+//import mockRouter from "next-router-mock";
 
-describe.skip("SellerForm", () => {
-  test("renders all input fields", () => {
+jest.mock("next/router", () => require("next-router-mock"));
+jest.mock("next-auth/react");
+
+describe("SellerForm", () => {
+  beforeEach(() => {
+    fetchMock.get("api/items/new", () => {
+      return [];
+    });
+    fetchMock.get("api/users/1", () => {
+      return {
+        firstName: "Alina",
+        lastName: "Zatzick",
+        email: "azatzick@middlebury.edu",
+        reviewerStatus: true,
+      };
+    });
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+    fetchMock.reset();
+  });
+
+  test("Client side rendering of seller form", () => {
+    SessionProvider.mockImplementation(({ children }) => (
+      <mock-provider>{children}</mock-provider>
+    ));
+    useSession.mockReturnValue({
+      data: {
+        user: { id: 1 },
+        expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      },
+      status: "authenticated",
+    });
+    render(<SellerPage />);
+    //expect(useSession).toBeCalledWith({ required: true });
+    expect(screen.getByText(/Sell your stuff!/i)).toBeInTheDocument();
+  });
+
+  test.skip("renders all input fields", () => {
     render(<SellerForm />);
     expect(screen.getByPlaceholderText("Item Name")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("Item Price")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("Item Description")).toBeInTheDocument();
   });
 
-  test("enables the Save button only when all fields are populated", () => {
+  test.skip("enables the Save button only when all fields are populated", () => {
     render(<SellerForm />);
     const nameInput = screen.getByPlaceholderText("Item Name");
     const priceInput = screen.getByPlaceholderText("Item Price");
