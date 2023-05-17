c5518fa55d6f2296564bdca1f472a0ff6b6365b7
Julian Calder <jcalder@middlebury.edu>
2023-05-17 10:39:28 -0400

    Fixed itemview tests and verified conditional edit button rendering

--- src/__tests__/itempage.test.js
+++ src/__tests__/itempage.test.js
@@ -1,22 +1,22 @@
 import { render, act } from "@testing-library/react";
 import IndividualItemView from "../components/IndividualItemView";
 import fetchMock from "fetch-mock-jest";
+import { useSession, SessionProvider } from "next-auth/react";
 
 jest.mock("next/router", () => require("next-router-mock"));
 jest.mock("next-auth/react");
 
 describe("IndividualItemView", () => {
   beforeEach(() => {
-    fetchMock.get("/api/users/1", () => {
-      return {
-        firstName: "Alina",
-        lastName: "Zatzick",
-        email: "azatzick@middlebury.edu",
-        reviewerStatus: true,
-      };
-    });
-    fetchMock.get("/api/items", () => {
-      return [];
+    SessionProvider.mockImplementation(({ children }) => (
+      <mock-provider>{children}</mock-provider>
+    ));
+    useSession.mockReturnValue({
+      data: {
+        user: { id: 1 },
+        expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      },
+      status: "authenticated",
     });
   });
   afterEach(() => {
@@ -24,7 +24,7 @@ describe("IndividualItemView", () => {
     fetchMock.reset();
   });
 
-  test.skip("renders item name", async () => {
+  test("renders item name", async () => {
     const item = {
       name: "Test Item",
       price: 10,
@@ -45,7 +45,7 @@ describe("IndividualItemView", () => {
     });
   });
 
-  test.skip("renders item price", async () => {
+  test("renders item price", async () => {
     const item = {
       name: "Test Item",
       price: 10,
@@ -66,7 +66,7 @@ describe("IndividualItemView", () => {
     });
   });
 
-  test.skip("renders item description", async () => {
+  test("renders item description", async () => {
     const item = {
       name: "Test Item",
       price: 10,
@@ -81,6 +81,50 @@ describe("IndividualItemView", () => {
     };
     const { getByText } = render(<IndividualItemView item={item} />);
     expect(getByText(/Test Description/)).toBeInTheDocument();
+
+    await act(async () => {
+      await new Promise(process.nextTick);
+    });
+  });
+
+  test("edit button doesn't appear when viewing other person's item", async () => {
+    const item = {
+      name: "Test Item",
+      price: 10,
+      description: "Test Description",
+      id: 1,
+      sellerId: 4,
+      sellerEmail: "mtarantola@middlebury.edu",
+      datePosted: "2016-12-07T23:22:33.357Z",
+      isAvailable: true,
+      images: "listings/igcyn5wksqm2xyurxyeg",
+      adminRemoved: false,
+    };
+
+    const { queryByText } = render(<IndividualItemView item={item} />);
+    expect(queryByText(/Edit item/)).not.toBeInTheDocument();
+
+    await act(async () => {
+      await new Promise(process.nextTick);
+    });
+  });
+
+  test("edit button appears when viewing your own item", async () => {
+    const item = {
+      name: "Test Item",
+      price: 10,
+      description: "Test Description",
+      id: 1,
+      sellerId: 1,
+      sellerEmail: "mtarantola@middlebury.edu",
+      datePosted: "2016-12-07T23:22:33.357Z",
+      isAvailable: true,
+      images: "listings/igcyn5wksqm2xyurxyeg",
+      adminRemoved: false,
+    };
+
+    const { getByText } = render(<IndividualItemView item={item} />);
+    expect(getByText(/Edit item/)).toBeInTheDocument();
 
     await act(async () => {
       await new Promise(process.nextTick);
