752ccc29384725f1e94e1d19f0b0199a6842171a
ivoverzone <iverzone@middlebury.edu>
2023-07-25 19:45:01 -0400

    fixed editor ux

--- src/components/IndividualItemView.js
+++ src/components/IndividualItemView.js
@@ -4,7 +4,7 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { Button, Box } from "@mui/material";
+import { Button, Box, useMediaQuery } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
@@ -13,44 +13,111 @@ export default function IndividualItemView({ item }) {
   const { data: session } = useSession();
   const public_id = item.images;
   const router = useRouter();
+
+  // Add this line to get the current breakpoint
+  const isScreenLarge = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
   return (
-    <Grid container spacing={2} justifyContent="center" justify="center">
-      <Grid item xs={12} sm="auto" alignItems="center">
-        <Card sx={{ maxWidth: "400px" }}>
-          <CardMedia
-            component="img"
-            style={{ height: "400" }}
-            image={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUD_NAME}/image/upload/${public_id}`}
-          />
-        </Card>
-      </Grid>
-      <Grid item xs={12} sx={{ pb: 3, pr: 3 }}>
-        <Container disableGutters sx={{ ml: 1 }}>
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <Typography variant="h4" align="left" style={{ fontWeight: 300 }}>
-              {item.name}
-            </Typography>
-            <Typography variant="h4" align="right" style={{ fontWeight: 300 }}>
-              ${item.price}
-            </Typography>
-          </Box>
-          <Typography variant="subtitle1" align="left">
-            {item.description}
-          </Typography>
-          {session && item && session.user.id === item.sellerId && (
-            <Button
-              size="medium"
-              sx={{ my: 2 }}
-              variant="outlined"
-              onClick={() => {
-                router.push(`/items/${item.id}/edit`);
-              }}
-            >
-              Edit item
-            </Button>
-          )}
-        </Container>
-      </Grid>
+    <Grid container spacing={2} justifyContent="flex-start">
+      {isScreenLarge ? (
+        <>
+          <Grid item xs={12} sm={6}>
+            <Card sx={{ pr: 0 }}>
+              <CardMedia
+                component="img"
+                image={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUD_NAME}/image/upload/${public_id}`}
+              />
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Container>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  align="left"
+                  style={{ fontWeight: 300 }}
+                >
+                  {item.name}
+                </Typography>
+                <Typography
+                  variant="h5"
+                  align="right"
+                  style={{ fontWeight: 400 }}
+                >
+                  ${item.price}
+                </Typography>
+              </Box>
+              <Typography variant="subtitle1" align="left">
+                {item.description}
+              </Typography>
+              {session && item && session.user.id === item.sellerId && (
+                <Button
+                  size="medium"
+                  sx={{ my: 2 }}
+                  variant="outlined"
+                  onClick={() => {
+                    router.push(`/items/${item.id}/edit`);
+                  }}
+                >
+                  Edit item
+                </Button>
+              )}
+            </Container>
+          </Grid>
+        </>
+      ) : (
+        <>
+          <Grid item xs={12} sm="auto" alignItems="center">
+            <Card sx={{ maxWidth: "400px" }}>
+              <CardMedia
+                component="img"
+                style={{ height: "400px" }}
+                image={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUD_NAME}/image/upload/${public_id}`}
+              />
+            </Card>
+          </Grid>
+          <Grid item xs={12} sx={{ pb: 3, pr: 3 }}>
+            <Container sx={{ ml: 1 }} disableGutters>
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Typography
+                  variant="h5"
+                  align="left"
+                  style={{ fontWeight: 300 }}
+                >
+                  {item.name}
+                </Typography>
+                <Typography
+                  variant="h5"
+                  align="right"
+                  style={{ fontWeight: 300 }}
+                >
+                  ${item.price}
+                </Typography>
+              </Box>
+              <Typography variant="subtitle1" align="left">
+                {item.description}
+              </Typography>
+              {session && item && session.user.id === item.sellerId && (
+                <Button
+                  size="medium"
+                  sx={{ my: 2 }}
+                  variant="outlined"
+                  onClick={() => {
+                    router.push(`/items/${item.id}/edit`);
+                  }}
+                >
+                  Edit item
+                </Button>
+              )}
+            </Container>
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 }
