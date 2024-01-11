7221331c94083c368f49c288d923411a1f8e820d
ivoverzone <iverzone@middlebury.edu>
2024-01-10 20:07:30 -0500

    add process.env

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -49,7 +49,7 @@ export default function App({
             {/* Global Site Tag (gtag.js) - Google Analytics */}
             <Script
               strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
             />
             <Script
               id="gtag-init"
@@ -60,7 +60,7 @@ export default function App({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
